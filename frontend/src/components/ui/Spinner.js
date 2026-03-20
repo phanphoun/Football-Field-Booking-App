ac828b4d21987bcd25ca74bf1758d82b8f7df709
PHOUN.PHAN <PHOUN.PHAN@PNC.LOCAL>
2026-03-20 07:43:38 +0700

    Remove current branch contents before merge

--- frontend/src/components/ui/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Spinner = ({ className = '' }) => (
-  <div className={`animate-spin rounded-full border-2 border-gray-200 border-t-green-600 ${className}`} />
-);
-
-export default Spinner;
-
